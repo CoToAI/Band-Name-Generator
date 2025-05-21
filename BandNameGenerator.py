f84a7508ee28c312dba9cd499c6f6dfc73560881
CoToAI <coreytownsend12@Gmail.com>
2025-05-21 13:16:44 -0400

    Update BandNameGenerator.py

---
+++ BandNameGenerator.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator")
+print("What's the name of the city you grew up in?")
+city = input()
+print("What's the name of your pet?")
+petName = input()
+print("What month were you born in?")
+birthdayMonth = input()
+
+print("Your band name could be " + birthdayMonth + " " + petName + " " + city)
